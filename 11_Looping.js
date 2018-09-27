567f8ecfb7667a2c12b14f01d94945681aa445da
siddharthsachdeva2835 <siddharthsachdeva2835@gmail.com>
2018-09-27 14:45:48 +0530

    looping with v-for

---
+++ 11_Looping.js
@@ -0,0 +1,21 @@
+new Vue({
+    el:'#app',
+    data : {
+        name:'siddharth',
+        characters: ['vishnu','shankar','rajesh'],
+        persons:[
+            {
+                name:"akshay",
+                age:24
+            },
+            {
+                name:"megha",
+                age:23
+            },
+            {
+                name:"dazzy",
+                age:22
+            }
+        ]
+    }
+});
